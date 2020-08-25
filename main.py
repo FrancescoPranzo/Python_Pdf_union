9b0531d9b1bd44eeceac8a9dd7872a8a43ac30aa
FrancescoPranzo <52277710+FrancescoPranzo@users.noreply.github.com>
2020-08-25 22:51:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import PyPDF2
+import tkinter as tk
+
+window = tk.Tk()
+#print(help(tk.Tk))
+window.geometry("600x300")
+window.title("PDF tool")
+
+r=5
+c=6
+print(type(r))
+tk.Button(text="click", borderwidth=1).grid(row=r, column=c)
+
+
+
+
+
+if  __name__ == "__main__":
+    window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
