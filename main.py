c041dd76f0fafbacf11881459437ddf77254b813
FrancescoPranzo <52277710+FrancescoPranzo@users.noreply.github.com>
2020-08-28 22:14:19 +0200

    Update main.py
    This version of the main definetly merge pdf.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import PyPDF2
 import tkinter as tk
 from tkinter import filedialog
-from object import pdf
-
+import random
 
 
 window = tk.Tk()
@@ -23,17 +22,32 @@ def file_choose():
 
 
 def generate_file_pdf():
-    print("sono nella funzione strana")
-
+    name_new_file = "file_unito"+str(random.randint(1,70))+".pdf"
     global list_file_name
-    print(list_file_name)
-    #print(len(list_file_name))
-    pd = pdf("arrigo", list_file_name[0])
-    pd.open_myfile_read()
+    print("sono nella funzione genera")
+    print("il nome del nuovo file è: " + str(name_new_file))
+    i = 0
+    print("il tipo di i è:" + str(type(i)))
+    
+    with open(name_new_file, "wb") as file_merged:
+        writer = PyPDF2.PdfFileWriter()
+        
+        print("sono nel with di scrittura")
+        
+        for i in list_file_name:
+            with open(i, "rb") as file_read:
+                print(i)
+                reader = PyPDF2.PdfFileReader(file_read, strict=False)
+                len_pdf = reader.getNumPages()
+                i = 0
+                for i in range(0, len_pdf):
+                    page = reader.getPage(i)
+                    writer.addPage(page)
+                writer.write(file_merged)
 
-
+    list_file_name = [ ]
 #aggiungere il parametro command
-Button1 = tk.Button(text="Choose another file", command=file_choose)
+Button1 = tk.Button(text="Choose the files", command=file_choose)
 Button1.grid(row=r, column=c)
 
 Button2 = tk.Button(text="Generate a file", borderwidth=2, command=generate_file_pdf)
@@ -43,4 +57,3 @@ Button2.grid(row=2, column=2)
 
 if  __name__ == "__main__":
     window.mainloop()
-#work in progress
